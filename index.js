472b2ff0ab2d172a56dab71e522813f8cb610728
TroddenSpade <parsasam1378@gmail.com>
2019-11-09 23:53:12 +0330

    request-promise-native

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const app = express();
 const port = 8569;
 app.use(express.json());
 
-app.post("/back_off_b1tch_u_dont_wanna_t3st_me", (req, res) => {
-  let resData = d0_da_g3t(req.body);
-  console.log(req.body, resData);
+app.post("/back_off_b1tch_u_dont_wanna_t3st_me", async (req, res) => {
+  let resData = await d0_da_g3t(req.body);
   res.json({
-    stats: "done"
+    stats: resData
   });
 });
 
